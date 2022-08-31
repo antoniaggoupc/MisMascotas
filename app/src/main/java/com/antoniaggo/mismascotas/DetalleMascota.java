6398051d40e0cf332f1e10d885dcf2ddf54ed6e7
Antonia <antonia.gallardo.gomez@gmail.com>
2022-08-31 10:22:17 +0200

    Initial commit with 3 layouts

---
+++ app/src/main/java/com/antoniaggo/mismascotas/DetalleMascota.java
@@ -0,0 +1,7 @@
+package com.antoniaggo.mismascotas;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class DetalleMascota extends AppCompatActivity {
+
+}
---
+++ app/src/main/java/com/antoniaggo/mismascotas/ListadoMascotas.java
@@ -0,0 +1,6 @@
+package com.antoniaggo.mismascotas;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class ListadoMascotas extends AppCompatActivity {
+}
